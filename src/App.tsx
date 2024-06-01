c384783d32d45e5f445751708e42af914d28b5a2
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-06-01 12:44:56 +0200

    template for chrome extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import { Heading, Text } from "@radix-ui/themes";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inputColorRef = useRef<HTMLInputElement>(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className="min-w-[300px] grid place-items-center">
+      <div className="flex flex-col gap-4">
+        <Heading as="h1">TLDRify this page</Heading>
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          onChange={summarise}
+          className="flex flex-col gap-4 items-center"
+        >
+          <Text className="flex gap-1" as="label" htmlFor="color">
+            <span>BG color</span>
+            <input ref={inputColorRef} name="color" id="color" type="color" />
+          </Text>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
+
+  async function summarise() {
+    const [tab] = await chrome.tabs.query({ active: true });
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id! },
+      args: [inputColorRef.current!.value],
+      func: (color) => {
+        document.body.style.backgroundColor = color;
+      },
+    });
+  }
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "@radix-ui/themes/styles.css";
+import "./globals.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Theme } from "@radix-ui/themes";
+
+import App from "./App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Theme>
+      <App />
+    </Theme>
+  </React.StrictMode>
+);
