6571152652be206caebd7e989d7c0c1b82de304c
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-06-01 14:28:42 +0200

    mvp tldrify

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,54 @@
-import { useRef } from "react";
-import { Heading, Text } from "@radix-ui/themes";
+import { useState } from "react";
+import { Button, Heading, Text } from "@radix-ui/themes";
+import { Readability } from "@mozilla/readability";
+import styles from "./App.module.scss";
 
 function App() {
-  const inputColorRef = useRef<HTMLInputElement>(null);
-
+  const [content, setContent] = useState<string>("");
   return (
     <section className="min-w-[300px] grid place-items-center">
       <div className="flex flex-col gap-4">
         <Heading as="h1">TLDRify this page</Heading>
-        <form
-          onSubmit={(e) => e.preventDefault()}
-          onChange={summarise}
-          className="flex flex-col gap-4 items-center"
-        >
-          <Text className="flex gap-1" as="label" htmlFor="color">
-            <span>BG color</span>
-            <input ref={inputColorRef} name="color" id="color" type="color" />
-          </Text>
-        </form>
+        <Button onClick={summarise}>Toggle zen view</Button>
+        {content.length ? (
+          <div
+            className={`prose max-w-none w-full overflow-auto ${styles.zen}`}
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
+        ) : (
+          <Text role="alert">Could not toggle zen mode</Text>
+        )}
       </div>
     </section>
   );
 
   async function summarise() {
-    const [tab] = await chrome.tabs.query({ active: true });
-
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id! },
-      args: [inputColorRef.current!.value],
-      func: (color) => {
-        document.body.style.backgroundColor = color;
-      },
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
     });
+
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tab.id! },
+        func: () => document.documentElement.outerHTML,
+      },
+      async ([result]) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(result.result!, "text/html");
+        const reader = new Readability(doc);
+        const article = reader.parse();
+        setContent(article!.content);
+
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id! },
+          func: (content) => {
+            console.log(content);
+          },
+          args: [article?.textContent],
+        });
+      }
+    );
   }
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+import tailwindcssTypo from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [tailwindcssTypo],
   corePlugins: {
     preflight: false,
   },
